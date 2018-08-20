b5efaf7fcf8382d2777019efbe74ac42727c8c97
Arianna Kellogg <ari@truss.works>
2018-08-19 21:31:21 -0700

    starting the process to refactor from mongo to mysql

--- database/index.js
+++ database/index.js
@@ -1,49 +1,108 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/notes');
+var mysql = require('mysql');
 
-// var db = mongoose.connection;
-  // commenting this jawn out didn't change anything
-
-// create schema
-var noteSchema = new mongoose.Schema({
-  note: String
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'test'
 });
 
-// compile schema into a model
-var Note = mongoose.model('Note', noteSchema);
-
-// write to db
-/////////
-let save = (note) => {
-  console.log('☹️ we are inside save');
-
-  var newNote = new Note({ note : note });
-
-  newNote.save(function(err) {
-    console.log('can haz save?')
-    if (err) {
-      console.log('error, NO SVAE');
-      return handleError(err);
-    }
-  });
-}
-
-
-let findAll = (cb) => {
-  Note.find().exec((err, data) => {
-    if (err) {
-      console.log('error, NO SVAE');
-      return handleError(err);
+var selectAll = function(callback) {
+  connection.query('SELECT * FROM items', function(err, results, fields) {
+    if(err) {
+      callback(err, null);
     } else {
-      cb(data);
+      callback(null, results);
     }
   });
-}
+};
 
-// findAll(); // returns an s-ton of data in the terminal
+module.exports.selectAll = selectAll;
 
 
 
-// export db
-module.exports.save = save;
-module.exports.findAll = findAll;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// MYSQL IN THE TOP, MONGODB BELOW
+
+
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/notes');
+
+// // var db = mongoose.connection;
+//   // commenting this jawn out didn't change anything
+
+// // create schema
+// var noteSchema = new mongoose.Schema({
+//   note: String
+// });
+
+// // compile schema into a model
+// var Note = mongoose.model('Note', noteSchema);
+
+// // write to db
+// /////////
+// let save = (note) => {
+//   console.log('☹️ we are inside save');
+
+//   var newNote = new Note({ note : note });
+
+//   newNote.save(function(err) {
+//     console.log('can haz save?')
+//     if (err) {
+//       console.log('error, NO SVAE');
+//       return handleError(err);
+//     }
+//   });
+// }
+
+
+// let findAll = (cb) => {
+//   Note.find().exec((err, data) => {
+//     if (err) {
+//       console.log('error, NO SVAE');
+//       return handleError(err);
+//     } else {
+//       cb(data);
+//     }
+//   });
+// }
+
+// // findAll(); // returns an s-ton of data in the terminal
+
+
+
+// // export db
+// module.exports.save = save;
+// module.exports.findAll = findAll;
